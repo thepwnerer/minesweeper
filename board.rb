f591bd5b6f1c4e33d19e2f182b1e066e10cf960f
ThePwnerer <maxpwong@gmail.com>
2020-12-30 11:21:18 -0800

    created grid initialization

--- board.rb
+++ board.rb
@@ -1,3 +1,36 @@
+require_relative 'tile'
+
 class Board
 
+    def initialize
+        skele_grid = Array.new(9) { Array.new(9) }
+        grid = Array.new
+        skele_grid.each_with_index.map { |row, i|
+            row.each_with_index.map { |tile, j|
+            #this puts a bomb in 20% of tiles
+            random = rand(5)
+            if random == 0
+                tile = Tile.new(Array.new << i << j, true)
+            else
+                tile = Tile.new(Array.new << i << j)
+            end
+            grid << tile } }
+        @grid = grid
+        
+    end
+
+    attr_reader :grid
+
+    def render
+        @grid.each do |tile|
+            if tile.revealed == false
+                p "*"
+            end
+        end
+    end
+
 end
+
+test = Board.new
+
+test.render
--- tile.rb
+++ tile.rb
@@ -1,6 +1,12 @@
 class Tile
-    def initialize(pos)
+
+    def initialize(pos, bomb = false)
         @position = pos
-        
+        @bomb = bomb
+        @flag = false
+        @revealed = false
     end
+
+    attr_reader :position
+    attr_accessor :bomb, :flag, :revealed
 end
